45926f8845a8eb6e96f9602a23bc7820e61510ad
nawhgnues <seungdang123jp@gmail.com>
2023-02-28 00:17:57 +0900

    set up next,ts,tailwindcss

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-gray-500">
+      <h1 className="text-green-500">it works</h1>
+    </div>
+  );
+};
+
+export default Home;
